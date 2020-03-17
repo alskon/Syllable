a17d8225d05573afea820c8447a17ddd21ee5dff
Alexey <askononenko@mail.ru>
2020-03-17 16:53:46 +0300

    hide firebase API

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrq2X2RujsydBw5_FuavOhLAbB79jAnek",
-    authDomain: "studyapp-7ab19.firebaseapp.com",
-    databaseURL: "https://studyapp-7ab19.firebaseio.com",
-    projectId: "studyapp-7ab19",
-    storageBucket: "studyapp-7ab19.appspot.com",
-    messagingSenderId: "540234084696",
-    appId: "1:540234084696:web:ff5396b2cbbd3d8124352d"
+    apiKey: "******",
+    authDomain: "******",
+    databaseURL: "******",,
+    projectId: "******",,
+    storageBucket: "******",,
+    messagingSenderId: "******",
+    appId: "******",
   };
 
 
