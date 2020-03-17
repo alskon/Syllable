ecabec97fee45705bee7110a054bda8b93ee38f9
Alexey <askononenko@mail.ru>
2020-03-17 20:01:29 +0300

    upload index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import{allReducers} from './store/reducers.js'
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(allReducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+            <App />
+    </Provider>, 
+    document.getElementById('root'));
+
+
+serviceWorker.unregister();
